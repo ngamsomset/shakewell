f8f3039b36e9f888652311bab22b1d99e125b791
Setthasiri Ngamsom <ngamsomset@Setthasiris-MacBook-Pro.local>
2021-05-15 19:54:43 +1000

    add link to each blog post

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import style from './Bloglist.module.css'
 import Blog from './Blog'
+import { Link } from 'react-router-dom'
 
 const BlogList = ({ data }) => {
   return (
     <div className={style.blog}>
       {data.map((blog) => (
-        <Blog
-          key={blog.id}
-          title={blog.title}
-          excerpt={blog.excerpt}
-          created={blog.created_at}
-        />
+        <Link to={`/article/${blog.id}`}>
+          <Blog
+            key={blog.id}
+            title={blog.title}
+            excerpt={blog.excerpt}
+            created={blog.created_at}
+          />
+        </Link>
       ))}
     </div>
   )
