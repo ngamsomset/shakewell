4fe092dbad4cd6afa9aadc43f08dda8bfa41c268
Setthasiri Ngamsom <ngamsomset@Setthasiris-MacBook-Pro.local>
2021-05-15 19:48:50 +1000

    add params

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router'
 import logo from './logo.png'
 
 const Article = (props) => {
+  const { id } = useParams()
+
   return (
     <div>
       <img src={logo} alt="logo"></img>
-      <h1>Contact</h1>
+      <h1>Contact {id}</h1>
     </div>
   )
 }
