3d303b136fb9c89c4b296f5c75562eb120e1031c
Setthasiri Ngamsom <ngamsomset@Setthasiris-MacBook-Pro.local>
2021-05-16 00:17:39 +1000

    add #tag to bloglist

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -18,6 +18,11 @@ const Blog = ({ title, created, excerpt }) => {
           <p>Published: {output}</p>
           <p>{excerpt}</p>
           <p>Author: John Doe</p>
+          <div>
+            <span>#design</span>
+            <span>#frontend</span>
+            <span>#backend</span>
+          </div>
         </div>
       </div>
     </div>
