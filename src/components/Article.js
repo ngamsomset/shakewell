d7e67b973383e27393ff9502528689f79f153409
Setthasiri Ngamsom <ngamsomset@Setthasiris-MacBook-Pro.local>
2021-05-16 12:34:07 +1000

    add return icon to article pages

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useParams } from 'react-router'
 import style from './Article.module.css'
+import { HiOutlineArrowCircleLeft } from 'react-icons/hi'
+import { IconContext } from 'react-icons'
 
 const Article = ({ data }) => {
   const { id } = useParams()
@@ -12,6 +14,21 @@ const Article = ({ data }) => {
   return (
     <div className={style.article}>
       <h2 className={style.heading}>{data[id].title}</h2>
+      <div>
+        <IconContext.Provider
+          value={{
+            style: {
+              color: '#fff',
+              fontSize: '3rem',
+              display: 'flex',
+              marginLeft: '3rem',
+              cursor: 'pointer',
+            },
+          }}
+        >
+          <HiOutlineArrowCircleLeft />
+        </IconContext.Provider>
+      </div>
       <section className={style.section}>
         <div dangerouslySetInnerHTML={createMarkUp(id)} />
       </section>
