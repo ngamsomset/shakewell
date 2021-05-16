81984ceb3d8b75f47f451bb2a5625615be7a21d5
Setthasiri Ngamsom <ngamsomset@Setthasiris-MacBook-Pro.local>
2021-05-16 12:17:54 +1000

    add react icon to blog component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from './logo.png'
 import style from './Blog.module.css'
+import { HiChevronRight } from 'react-icons/hi'
+import { IconContext } from 'react-icons'
 
 const Blog = ({ title, created, excerpt }) => {
   var myDate = new Date(created)
@@ -14,7 +16,13 @@ const Blog = ({ title, created, excerpt }) => {
           <img src={logo} alt="logo"></img>
         </div>
         <div className={style.contentwrap}>
-          <h2>{title}</h2>
+          <h2>
+            <IconContext.Provider
+              value={{ style: { verticalAlign: 'middle' } }}
+            >
+              {title} <HiChevronRight />
+            </IconContext.Provider>
+          </h2>
           <p>Published: {output}</p>
           <p>{excerpt}</p>
           <p>Author: John Doe</p>
